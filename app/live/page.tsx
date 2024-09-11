112ed9f3bd974a6f6239c57f84915ff6d8fd9d08
Vipin Das <satlokashram.vipin@gmail.com>
2024-09-11 10:00:15 +0530

    Solve error in youtube live

--- app/live/page.tsx
+++ app/live/page.tsx
@@ -5,8 +5,9 @@ import React from 'react'
 const LivePage = () => {
     return (
         <div>
-            <LiveSwither />
-            <YoutubeLive />
+            <YoutubeLive>
+                
+            </YoutubeLive>
         </div>
     )
 }
