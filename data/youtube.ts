cb84cc6b9829db9b03f37a0136a82ace4d460159
Vipin Das <satlokashram.vipin@gmail.com>
2024-09-11 10:04:18 +0530

    Updated Youtube Key

--- data/youtube.ts
+++ data/youtube.ts
@@ -3,8 +3,8 @@ import { VideoItem, VideoStatistics, VideoStatisticsResponse, YoutubeData, Video
 
 
 
-const API_KEY = 'AIzaSyChYwvuZFZOyiBM1OkPA7Fimu1XfI0d5SA';
-// const API_KEY = 'AIzaSyAPfS0kmoUVn_xREJPI8QMNbZL5jj7-6QE';
+// const API_KEY = 'AIzaSyChYwvuZFZOyiBM1OkPA7Fimu1XfI0d5SA';
+const API_KEY = 'AIzaSyAPfS0kmoUVn_xREJPI8QMNbZL5jj7-6QE';
 const CHANNEL_ID = 'UCtb33Ciysb6TEkXaORcVmpg'; // Sant Rampal Ji Maharaj
 
 
